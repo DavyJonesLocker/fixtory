ac4dc2beee1d943bd60ed6f114d5b687dd9da8c6
Brian Cardarella <bcardarella@gmail.com>
2014-09-17 18:00:16 -0400

    Insert data into database
    Allow the builder to insert into the database

---
+++ lib/fixtory/methods.rb
@@ -0,0 +1,8 @@
+require 'fixtory/dsl'
+
+module Fixtory::Methods
+  def fixtory(group_name)
+    builder = Fixtory::DSL.build_from(Fixtory.path_for(group_name))
+    builder
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,36 @@
 require 'minitest/autorun'
 require 'fixtory'
+require 'fixtory/methods'
 require 'byebug'
+require 'active_record'
+require 'database_cleaner'
+
+DatabaseCleaner.strategy = :truncation
+
+class Minitest::Spec
+  include Fixtory::Methods
+
+  before do
+    DatabaseCleaner.start
+  end
+
+  after do
+    DatabaseCleaner.clean
+  end
+end
+
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database => ':memory:'
+)
+
+ActiveRecord::Base.connection.execute(%{CREATE TABLE owners (id INTEGER PRIMARY KEY, name TEXT, age DOUBLE);})
+ActiveRecord::Base.connection.execute(%{CREATE TABLE dogs (id INTEGER PRIMARY KEY, name TEXT, age DOUBLE, owner_id INTEGER);})
+
+class Owner < ActiveRecord::Base
+
+end
+
+class Dog < ActiveRecord::Base
+
+end
