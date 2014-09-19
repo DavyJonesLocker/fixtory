a069f95eb0d6e0822e583d173ccecb90e34754c8
Brian Cardarella <bcardarella@gmail.com>
2014-09-19 15:50:15 -0400

    Make row basic object

--- lib/fixtory/dsl/row.rb
+++ lib/fixtory/dsl/row.rb
@@ -2,25 +2,26 @@ require 'active_support/deprecation'
 require 'active_support/concern'
 require 'active_record/fixtures'
 
-class Fixtory::DSL::Row
+class Fixtory::DSL::Row < BasicObject
   def initialize(name, table, &block)
     @name = name.to_s
     @table = table
     @attributes = {
-      'id' => ActiveRecord::FixtureSet.identify(@name)
+      'id' => ::ActiveRecord::FixtureSet.identify(@name)
     }
     instance_eval &block
   end
 
   def method_missing(attribute, *args)
     attribute = attribute.to_s
+
     if args.first
       @attributes[attribute] = args.first
     else
       if @attributes.key?(attribute)
         @attributes[attribute]
       else
-        @table._builder.send(attribute)
+        @table._builder.instance_eval(attribute)
       end
     end
   end
