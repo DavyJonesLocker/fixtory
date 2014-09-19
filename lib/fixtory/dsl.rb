269ca474021ddbd369a507067a46742f34ce11ae
Brian Cardarella <bcardarella@gmail.com>
2014-09-19 15:40:52 -0400

    Reduced syntax
    Uber simple

--- lib/fixtory/dsl.rb
+++ lib/fixtory/dsl.rb
@@ -1,7 +1,7 @@
 module Fixtory::DSL
   def self.build_from(path)
     builder = Fixtory::DSL::Builder.new
-    builder.eval_from_fixtory_file(path)
+    builder._eval_from_fixtory_file(path)
     builder
   end
 end
